e232a411e26b913d16a10b7402ef2d3668d8b9de
cjybb5413 <120069938@qq.com>
2018-03-20 12:57:54 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/RegisterActivity.java
@@ -0,0 +1,10 @@
+package cn.edu.gdmec.android.boxuegu;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Asus on 2018/3/20.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/SplashActivity.java
@@ -0,0 +1,45 @@
+package cn.edu.gdmec.android.boxuegu;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by Asus on 2018/3/19.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    private TextView tv_version;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        tv_version=findViewById(R.id.tv_version);
+        try {
+            PackageInfo info=getPackageManager().getPackageInfo(getPackageName(),0);
+            tv_version.setText("V"+info.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            tv_version.setText("V");
+        }
+        Timer timer=new Timer();
+        TimerTask task=new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        };
+        timer.schedule(task,3000);
+    }
+}
