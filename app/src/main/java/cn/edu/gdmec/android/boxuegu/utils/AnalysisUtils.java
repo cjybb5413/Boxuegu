279200846093e031fa1c2b78c9a4d4822d0d82a7
cjybb5413 <120069938@qq.com>
2018-04-10 12:20:06 +0800

    设置功能模

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/utils/AnalysisUtils.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/utils/AnalysisUtils.java
@@ -29,27 +29,5 @@ public class AnalysisUtils {
         SharedPreferences.Editor editor = sp.edit();
         editor.putBoolean("isLogin", false);
         editor.putString("loginUserName", "");
-=======
- * Created by apple on 18/4/3.
- */
-
-public class AnalysisUtils {
-    public static String readLoginUserName(Context context){
-        SharedPreferences sp=context.getSharedPreferences("loginInfo",Context.MODE_PRIVATE);
-        String userName=sp.getString("loginUserName","");
-        return userName;
-    }
-    public static boolean readLoginStatus(Context context){
-        SharedPreferences sp=context.getSharedPreferences("loginInfo", Context.MODE_PRIVATE);
-        boolean isLogin=sp.getBoolean("isLogin",false);
-        return isLogin;
-    }
-    public static void clearLoginStatus(Context context){
-        SharedPreferences sp=context.getSharedPreferences("loginInfo",Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor=sp.edit();
-        editor.putBoolean("isLogin",false);
-        editor.putString("loginUserName","");
->>>>>>> github/master
-        editor.commit();
     }
 }
